e5cb1b94a287ebdd68c741c848f4a6b3df0655dd
teddyboski <tedboy9@gmail.com>
2026-08-05 16:02:10 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,15 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dszbfvhpfnmpbhqgmzst.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
